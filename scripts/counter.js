481cfc313c5a9907ac2cee143a50c859941e75cc
jhamadhav <jhamadhav28@gmail.com>
2022-01-14 21:47:35 +0530

    faq improved

--- scripts/counter.js
+++ scripts/counter.js
@@ -48,4 +48,27 @@ window.onload = () => {
         //seconds
     }, 0)
 
+
+    let acc = document.getElementsByClassName("accordion");
+
+    for (let i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            for (let j = 0; j < acc.length; j++) {
+                if (acc[j].classList.contains("active")) {
+                    acc[j].classList.toggle("active")
+                    acc[j].nextElementSibling.style = "none"
+                }
+            }
+            this.classList.toggle("active");
+
+            let panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
+
+
 }
