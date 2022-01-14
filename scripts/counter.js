5efa3572acfaa4156d3d16265802d10aef8ce1fa
jhamadhav <jhamadhav28@gmail.com>
2022-01-14 21:54:43 +0530

    loader bg added

--- scripts/counter.js
+++ scripts/counter.js
@@ -53,12 +53,12 @@ window.onload = () => {
 
     for (let i = 0; i < acc.length; i++) {
         acc[i].addEventListener("click", function () {
-            for (let j = 0; j < acc.length; j++) {
-                if (acc[j].classList.contains("active")) {
-                    acc[j].classList.toggle("active")
-                    acc[j].nextElementSibling.style = "none"
-                }
-            }
+            // for (let j = 0; j < acc.length; j++) {
+            //     if (acc[j].classList.contains("active")) {
+            //         acc[j].classList.toggle("active")
+            //         acc[j].nextElementSibling.style = "none"
+            //     }
+            // }
             this.classList.toggle("active");
 
             let panel = this.nextElementSibling;
