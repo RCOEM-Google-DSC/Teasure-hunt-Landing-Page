a6b95140cd2431e475a7764cb6dccdbe927a1570
jhamadhav <jhamadhav28@gmail.com>
2022-01-26 11:31:12 +0530

    countdown updated

--- scripts/counter.js
+++ scripts/counter.js
@@ -7,21 +7,8 @@ window.onload = () => {
         hour = minute * 60,
         day = hour * 24;
 
-    let today = new Date(),
-        dd = String(today.getDate()).padStart(2, "0"),
-        mm = String(today.getMonth() + 1).padStart(2, "0"),
-        yyyy = today.getFullYear(),
-        nextYear = yyyy,
-        dayMonth = "01/27/",
-        birthday = dayMonth + yyyy;
-
-    today = mm + "/" + dd + "/" + yyyy;
-    if (today > birthday) {
-        birthday = dayMonth + nextYear;
-    }
-    //end
-
-    let countDown = new Date(birthday).getTime()
+    let eventDate = "27 Jan 2022 15:58"
+    let countDown = new Date(eventDate).getTime()
 
     setInterval(() => {
 
@@ -43,6 +30,8 @@ window.onload = () => {
 
             document.getElementsByClassName("countdown")[0].innerHTML = ""
 
+            document.getElementsByClassName("btn")[0].innerText = "Log in"
+            document.getElementById("btn-a").href = "https://dsc-treasure-hunt.herokuapp.com"
             clearInterval(x);
         }
         //seconds
